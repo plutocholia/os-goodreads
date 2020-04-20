acae80c2bce5a073c5988e69c3a6a6c9ffc03b4d
plutocholia <kiarash.nrz77@gmail.com>
2020-04-20 08:41:36 +0430

    Utills file is added

---
+++ Utills.h
@@ -0,0 +1,30 @@
+#ifndef UTILLS_H
+#define UTILLS_H
+
+#include <vector>
+#include <string>
+
+#define DIR_DATASETS    "./datasets/"
+#define DIR_REVIEW      "./datasets/reviews.csv"
+#define DIR_BOOKS       "./datasets/books.csv"
+
+class Utills
+{
+public:
+    static void removeSpace(std::string&);
+    static std::vector<std::string> splitBy(
+        std::string, const std::string&
+    );
+    static void printStringVector(
+        const std::vector<std::string>& vec
+    );
+    static void printPairStringVector(
+        const std::vector<std::pair<std::string, std::string> >&
+    );
+    static void printPairIdStringVector(
+        const std::vector<std::pair<int, std::string> >&
+    );
+};
+
+
+#endif // !UTILLS_H
