7110e94438dcf047beefa70a8150d3c7fcfa015c
plutocholia <kiarash.nrz77@gmail.com>
2020-04-23 17:31:05 +0430

    reading books and reviews files is added

--- Serial.cpp
+++ Serial.cpp
@@ -7,7 +7,7 @@
 #include "Utills.h"
 #include "Book.h"
 #include <iomanip>
-
+#include <string.h>
 Serial::Serial(const std::string& gener)
 {
     this->gener = gener;
@@ -17,56 +17,27 @@ Serial::~Serial()
 {
 }
 
+void Serial::readFiles(){
+    std::ifstream _file;
+    _file.open(DIR_BOOKS);
+    _file.seekg(0, std::ios_base::end);
+    this->size_books = _file.tellg();
+    _file.seekg(0);
+    this->data_books = new char[this->size_books];
+    _file.read(this->data_books, this->size_books);
+    _file.close();
+    _file.open(DIR_REVIEW);
+    _file.seekg(0, std::ios_base::end);
+    this->size_reviews = _file.tellg();
+    _file.seekg(0);
+    this->data_reviews = new char[this->size_reviews];
+    _file.read(this->data_reviews, this->size_reviews);
+    _file.close();
+    std::cout << strlen(this->data_books) << " " << strlen(this->data_reviews) << std::endl;
+}
+
+
+
 void Serial::run(){
-    std::cout << "Serial Filtering is running on Gener: " << this->gener << std::endl;
-    std::fstream ifile;
-    std::unordered_map<int, Book*> books;
-
-    ifile.open(DIR_BOOKS, std::ios::in);
-    if(ifile.is_open()){
-        std::string line;
-        size_t line_number = 0;
-        while(getline(ifile, line)){
-            if(!(line_number == 0)){
-                Book* book = new Book(Utills::splitBy(line, ","));
-                if(book->ifIncludeGener(this->gener))
-                    books.insert(std::make_pair(book->getBookID(), book));
-            }
-            line_number += 1;
-        }
-        ifile.close();
-    }
-    else{
-        std::cout << "Unable to open books.csv\n";
-        exit(EXIT_FAILURE);
-    }
-
-    ifile.open(DIR_REVIEW, std::ios::in);
-    Book* max_book = NULL;
-    float max_pop  = -1;
-    if(ifile.is_open()){
-        std::string line;
-        size_t line_number = 0;
-        while(getline(ifile, line)){
-            if(!(line_number == 0)){
-                std::vector<std::string> review = Utills::splitBy(line, ",");
-                int book_id         = std::atoi(review[0].c_str());
-                int rating          = std::atoi(review[1].c_str());
-                int number_of_likes = std::atoi(review[2].c_str());
-                std::unordered_map<int, Book*>::iterator itr_book = books.find(book_id);
-                if(itr_book != books.end())
-                    itr_book->second->updateRates(rating, number_of_likes);
-            }
-            line_number += 1;
-        }
-        ifile.close();
-    }
-
-    for(auto item : books){
-        if(item.second->getPopRate() > max_pop){
-            max_book = item.second;
-            max_pop = item.second->getPopRate();
-        }
-    }
-    max_book->printBook();
+    this->readFiles();  
 }
