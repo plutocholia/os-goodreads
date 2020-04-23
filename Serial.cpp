6e880d5f26d5059aa1287273471028e2e10cf90d
plutocholia <kiarash.nrz77@gmail.com>
2020-04-23 21:12:41 +0430

    Refinement of Serial and Book is added from Serial branch

--- Serial.cpp
+++ Serial.cpp
@@ -7,7 +7,7 @@
 #include "Utills.h"
 #include "Book.h"
 #include <iomanip>
-
+#include <string.h>
 Serial::Serial(const std::string& gener)
 {
     this->gener = gener;
@@ -17,56 +17,110 @@ Serial::~Serial()
 {
 }
 
-void Serial::run(){
-    std::cout << "Serial Filtering is running on Gener: " << this->gener << std::endl;
-    std::fstream ifile;
-    std::unordered_map<int, Book*> books;
+void Serial::readFiles(){
+    std::ifstream _file;
+    _file.open(DIR_BOOKS);
+    _file.seekg(0, std::ios_base::end);
+    this->size_books = _file.tellg();
+    _file.seekg(0);
+    this->data_books = new char[this->size_books];
+    _file.read(this->data_books, this->size_books);
+    _file.close();
+    _file.open(DIR_REVIEW);
+    _file.seekg(0, std::ios_base::end);
+    this->size_reviews = _file.tellg();
+    _file.seekg(0);
+    this->data_reviews = new char[this->size_reviews];
+    _file.read(this->data_reviews, this->size_reviews);
+    _file.close();
+}
 
-    ifile.open(DIR_BOOKS, std::ios::in);
-    if(ifile.is_open()){
-        std::string line;
-        size_t line_number = 0;
-        while(getline(ifile, line)){
-            if(!(line_number == 0)){
-                Book* book = new Book(Utills::splitBy(line, ","));
-                if(book->ifIncludeGener(this->gener))
-                    books.insert(std::make_pair(book->getBookID(), book));
-            }
-            line_number += 1;
+void Serial::parseBooks(){
+    // std::vector<std::string> lines;
+    // Utills::splitByRef(lines, this->data_books, "\n");
+
+    // for(auto& line : lines){
+    //     if(line.size() < 1) continue;
+    //     std::vector<std::string> temp;
+    //     Utills::splitByRef(temp, line, ",");
+    //     Book* book = new Book(temp);
+    //     if(book->ifIncludeGener(gener)){
+    //         this->books.insert(std::make_pair(book->getBookID(), book));
+    //     }
+    // }
+
+    // ------------------------------- V2 ----------------------------------
+
+    std::string data_scope("");
+    std::vector<std::string> item;
+    int i = 0;
+    while(this->data_books[i] != '\n') i++;
+    i++;
+    for(; i < this->size_books; i++){
+        if(this->data_books[i] == ','){
+            item.push_back(data_scope);
+            data_scope = "";
         }
-        ifile.close();
+        else if(this->data_books[i] == '\n'){
+            item.push_back(data_scope);
+            Book* book = new Book(item);
+            if(book->ifIncludeGener(gener))
+                this->books.insert(std::make_pair(book->getBookID(), book));
+            data_scope = "";
+            item.clear();
+        } 
+        else
+            data_scope += this->data_books[i];
     }
-    else{
-        std::cout << "Unable to open books.csv\n";
-        exit(EXIT_FAILURE);
-    }
+}
 
-    ifile.open(DIR_REVIEW, std::ios::in);
+void Serial::parseReviews(){
+    std::string data_scope("");
+    int scope = 0;
+    int* item = new int[3];
+    int i = 0;
+    while(this->data_reviews[i] != '\n') i++;
+    i++;
+    for(; i < this->size_reviews; i++){
+        if(this->data_reviews[i] == ','){
+            item[scope++] = std::stoi(data_scope.c_str());
+            data_scope = "";
+        }
+        else if(this->data_reviews[i] == '\n'){
+            item[scope] = std::stod(data_scope.c_str());
+            scope = 0;
+            this->reviews.push_back(item);
+            item = new int[3];
+            data_scope = "";
+        } 
+        else
+            data_scope += this->data_reviews[i];
+    }
+}
+
+void Serial::findBestBook(){
+    
+    for(auto& line : reviews){
+        std::unordered_map<int, Book*>::iterator itr_book = this->books.find(line[0]);
+        if(itr_book != books.end())
+            itr_book->second->updateRates(line[1], line[2]);
+    }
+    
     Book* max_book = NULL;
     float max_pop  = -1;
-    if(ifile.is_open()){
-        std::string line;
-        size_t line_number = 0;
-        while(getline(ifile, line)){
-            if(!(line_number == 0)){
-                std::vector<std::string> review = Utills::splitBy(line, ",");
-                int book_id         = std::atoi(review[0].c_str());
-                int rating          = std::atoi(review[1].c_str());
-                int number_of_likes = std::atoi(review[2].c_str());
-                std::unordered_map<int, Book*>::iterator itr_book = books.find(book_id);
-                if(itr_book != books.end())
-                    itr_book->second->updateRates(rating, number_of_likes);
-            }
-            line_number += 1;
-        }
-        ifile.close();
-    }
-
-    for(auto item : books){
+    for(auto& item : books){
         if(item.second->getPopRate() > max_pop){
             max_book = item.second;
             max_pop = item.second->getPopRate();
         }
     }
+
     max_book->printBook();
+}
+
+void Serial::run(){
+    this->readFiles();
+    this->parseBooks();  
+    this->parseReviews();
+    this->findBestBook();
 }
--- Serial.h
+++ Serial.h
@@ -1,13 +1,26 @@
 #ifndef SERIAL_H
 #define SERIAL_H
 #include <string>
-
+#include <unordered_map>
+#include <vector>
+#include "Book.h"
 class Serial
 {
 private:
     std::string gener;
+    std::unordered_map<int, Book*> books;
+    std::vector<int*> reviews;
+    char* data_books;
+    char* data_reviews;
+    size_t size_books;
+    size_t size_reviews;
+
 public:
     Serial(const std::string&);
+    void readFiles();
+    void parseBooks();
+    void parseReviews();
+    void findBestBook();
     ~Serial();
     void run();
 };
